ed45f8b572038f3824d77242ecb29f31b902b250
이종학 <jonghak@jonghak.local>
2021-08-11 09:34:17 +0900

    DB 조회 endpoint 추가

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
   @GetMapping("/")
   public ResponseEntity<String> hello() {
     return ResponseEntity.ok("Hello World!");
+  }
+
+  @GetMapping("/menus")
+  public ResponseEntity<List<Map<String, Object>>> menus() {
+    List<Map<String, Object>> menus = jdbcTemplate.queryForList("SELECT * FROM lunch_menu");
+    return ResponseEntity.ok(menus);
   }
 }
